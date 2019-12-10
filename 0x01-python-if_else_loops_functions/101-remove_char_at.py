bd6ab73b367926ec009f948c422da8bf32636531
Andresmelek <candres.isaza@gmail.com>
2019-12-10 16:52:17 -0500

    jumping

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str1 = ''
+    index = 0
+    for i in str:
+        if index == n:
+            index += 1
+        else:
+            str1 = chr(ord(i))
+            index += 1
+    return str1
