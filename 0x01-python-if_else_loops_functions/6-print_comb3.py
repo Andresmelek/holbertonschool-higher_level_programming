4842cb3d76b863a8a725b36e465005171ff244e9
Andresmelek <candres.isaza@gmail.com>
2019-12-10 13:25:07 -0500

    Combination with the smallest combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for y in range(1, 10):
+        if i < y:
+            print('{}{}, '.format(i, y), end='')
+        if i == 8 and y == 9:
+            print('{}{}'.format(i, y))
