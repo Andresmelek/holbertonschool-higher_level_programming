d7ebf96051bdc643b8dee972c568200af76f7fd8
Andresmelek <candres.isaza@gmail.com>
2019-12-10 13:29:43 -0500

    Fixing task #6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for y in range(1, 10):
-        if i < y:
-            print('{}{}, '.format(i, y), end='')
         if i == 8 and y == 9:
             print('{}{}'.format(i, y))
+            break
+        if i < y:
+            print('{}{}, '.format(i, y), end='')
