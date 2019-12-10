1ec42adffd75f78bc7367440a1ab33d53b2a9f42
Andresmelek <candres.isaza@gmail.com>
2019-12-10 15:01:42 -0500

    Convertin in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(c) >= 97 and ord(c) <= 122:
+            str1 = ord(c) - 32
+        print('{}'.format(chr(str1)), end='')
