a3646dc36c5f9712b856c16ec7d3cfc3472bce77
Andresmelek <candres.isaza@gmail.com>
2019-12-12 12:55:54 -0500

    Fixing task #2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     argv = (sys.argv)
     number = len(argv)
     if number == 2:
-        print('{} argument'.format(number - 1))
+        print('{} argument:'.format(number - 1))
         print('1: {}'.format(argv[1]))
     elif number == 1:
-        print('{} arguments'.format(number - 1))
+        print('{} arguments.'.format(number - 1))
     else:
-        print('{} arguments'.format(number - 1))
+        print('{} arguments:'.format(number - 1))
         for i in range(1, number):
             print('{}: {}'.format(i, argv[i]))
