9adfcf6101b2f67fe7dbd139766926e28ca636ad
Andresmelek <candres.isaza@gmail.com>
2019-12-12 16:20:01 -0500

    Fixing task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,23 @@ from calculator_1 import add, sub, mul, div
 import sys
 if __name__ == "__main__":
     argv = sys.argv
+
     if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
+        sys.exit(1)
+
     a = int(argv[1])
     c = argv[2]
     b = int(argv[3])
 
     if c == '+':
-        print('{} {} {} = {}'.format(a, c, b, add(a, b)))
+        print('{} + {} = {}'.format(a, b, add(a, b)))
     elif c == '-':
-        print('{} {} {} = {}'.format(a, c, b, sub(a, b)))
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
     elif c == '*':
-        print('{} {} {} = {}'.format(a, c, b, mul(a, b)))
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
     elif c == '/':
-        print('{} {} {} = {}'.format(a, c, b, div(a, b)))
+        print('{} / {} = {}'.format(a, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+        sys.exit(1)
