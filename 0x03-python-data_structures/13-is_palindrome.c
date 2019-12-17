2765662015e074e9a0bb6ff38e78742a74aef9e2
Andresmelek <candres.isaza@gmail.com>
2019-12-17 17:55:43 -0500

    Is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * is_palindrome - function in C that checks if a singly linked list is a palindrome.
+ * @head: double pointer to the first node
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *aux;
+	int i = 0, len = 0, j = 0;
+	int buff[1000];
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	aux = *head;
+	while(aux)
+	{
+		buff[i] = aux->n;
+		i++;
+		aux = aux->next;
+	}
+	len = i - 1;
+	while (j < len)
+	{
+		if (buff[len] != buff[j])
+			return (0);
+		len--;
+		j++;
+	}
+	return (1);
+}
