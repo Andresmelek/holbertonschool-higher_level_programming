89d90fe7b697102a015b1ba3a1a0c5633cb476e6
Andresmelek <candres.isaza@gmail.com>
2019-12-19 17:52:34 -0500

    Trying another output

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     matrix = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string in matrix or roman_string:
+    if isinstance(roman_string, str) or roman_string:
         number = matrix[roman_string[len(roman_string) - 1]]
         for i in range((len(roman_string) - 1), 0, - 1):
             one = matrix[roman_string[i]]
