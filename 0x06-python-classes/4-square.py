ad7f7dcba72a4aed4027bbeaf5470e0f17006b65
Andresmelek <candres.isaza@gmail.com>
2020-01-09 18:29:12 -0500

    Fixing output

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,4 +22,8 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if type(value) is not int:
+            raise TypeError("{}".format("size must be an integer"))
+        if value < 0:
+            raise ValueError("{}".format("size must be >= 0"))
         self.__size = value
