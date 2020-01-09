fa30253714435a752b0235328058d008d48b7e0b
Andresmelek <candres.isaza@gmail.com>
2020-01-08 22:09:06 -0500

    Error of integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {:s}".format(str(error)), file=sys.stderr)
+        return False
