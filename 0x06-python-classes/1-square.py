121c735a69f3de42f18cfbb115291642f8d0530a
Andresmelek <candres.isaza@gmail.com>
2020-01-09 17:24:15 -0500

    Second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+
+        """ class Square
+
+        The __init__ creates a square with one attribute.
+
+        Attributes:
+        size (int): private instance attribute."""
+
+        self.__size = size
