8dd2ffe519d825cb8a373bf7c8922a600e254928
Andresmelek <candres.isaza@gmail.com>
2020-01-13 17:24:24 -0500

    Adding test cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,25 +12,29 @@ def matrix_divided(matrix, div):
     """
     Check conditionals
     """
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    matriz = len(matrix)
+    msg = "matrix must be a matrix (list of lists) of integers/float"
+    msg1 = "Each row of the matrix must have the same size"
+    matriz = len(matrix[0])
     matrix_div = []
-    if not isinstance(matrix(int, float)):
-        raise TypeError(msg)
     if matrix is None:
         raise TypeError(msg)
+    if type(div) is not int or type(div) is not float:
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    if not isinstance(matrix(int, list)):
+    if type(matrix) is not int and type(matrix) is not list:
         raise TypeError(msg)
 
-    for i in range(len(matrix)):
-        if len(matrix[i]) != matriz:
-            raise TypeError(msg)
+    for x in range(len(matrix)):
+        if len(matrix[x]) != matriz:
+            raise TypeError(msg1)
 
-    for y in i:
-        if not isinstance(y, (int, list)):
+    for x in matrix:
+        if not isinstance(matrix, list):
             raise TypeError(msg)
+        for y in x:
+            if type(y) is not int or type(y) is not float:
+                raise TypeError(msg)
 
     matrix_div = [[round(y / div, 2) for y in i] for i in matrix]
     return matrix_div
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """
 function that prints a text with 2 new lines after each of these
 characters: ., ? and :
@@ -6,9 +6,13 @@ characters: ., ? and :
 
 
 def text_indentation(text):
-    if not isinstance(text, str):
+    """
+    Function that prints  a text with
+    2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
         raise TypeError('text must be a string')
     text1 = text.replace('?', '?\n\n')
-    text1 = text.replace('.', '?\n\n')
-    text1 = text.replace(':', '?\n\n')
+    text1 = text1.replace('.', '?\n\n')
+    text1 = text1.replace(':', '?\n\n')
     print("\n".join([i.strip() for i in text1.split("\n")]), end="")
