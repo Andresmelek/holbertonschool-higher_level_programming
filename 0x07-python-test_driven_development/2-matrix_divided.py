d2afb37b091a6ec35b7f3a331508152b00d7b5dc
Andresmelek <candres.isaza@gmail.com>
2020-01-13 09:32:40 -0500

    Changes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,7 +14,7 @@ def matrix_divided(matrix, div):
     """
     msg = "matrix must be a matrix (list of lists) of integers/floats"
     matriz = len(matrix)
-    matrix_div
+    matrix_div = []
     if not isinstance(matrix(int, float)):
         raise TypeError(msg)
     if matrix is None:
@@ -28,4 +28,9 @@ def matrix_divided(matrix, div):
         if len(matrix[i]) != matriz:
             raise TypeError(msg)
 
+    for y in i:
+        if not isinstance(y, (int, list)):
+            raise TypeError(msg)
 
+    matrix_div = [[round(y / div, 2) for y in i] for i in matrix]
+    return matrix_div
