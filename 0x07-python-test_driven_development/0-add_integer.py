3fb826c01f4c4a15ff65f6e2a22d0953bc0b6489
Andresmelek <candres.isaza@gmail.com>
2020-01-13 23:45:30 -0500

    Changes finales

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,14 +12,16 @@ def add_integer(a, b=98):
     """
     if a is None:
         raise TypeError("a must be an integer")
-    if b is None:
+    elif b is None:
         raise TypeError("b must be an integer")
-    if type(a) is not int and type(a) is not float:
+    elif type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if type(a) is float:
+    elif type(a) is float:
         a = int(a)
-    if type(b) is float:
+    elif type(b) is float:
         b = int(b)
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
+        raise OverflowError("Number too large")
     return a + b
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,42 +14,37 @@ def matrix_divided(matrix, div):
     """
     msg = "matrix must be a matrix (list of lists) of integers/float"
     msg1 = "Each row of the matrix must have the same size"
-    matriz = len(matrix[0])
     matrix_div = []
-
-    if matrix is None:
-        raise TypeError(msg)
 
     if matrix == []:
         raise TypeError(msg)
 
-    if type(div) is not int or type(div) is not float:
+    elif matrix is None:
+        raise TypeError(msg)
+
+    elif type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError('division by zero')
 
-    if type(matrix) is not int or type(matrix) is not list:
+    elif not isinstance(matrix, list) and isinstance(matrix, list):
         raise TypeError(msg)
 
-    if not isinstance(matrix[0], list):
+    elif not isinstance(matrix[0], list):
         raise TypeError(msg)
-
-    for x in range(len(matrix)):
-
-        if not isinstance(matriz[x], list)):
-            raise TypeError(msg)
-
-        if len(matrix[x]) != matriz:
-            raise TypeError(msg1)
 
     for x in matrix:
-        if not isinstance(matrix, list):
+
+        if not isinstance(x, list):
             raise TypeError(msg)
 
-        for y in x:
-            if type(y) is not int or type(y) is not float:
+        if len(x) != len(matrix[0]):
+            raise TypeError(msg1)
+
+        for z in x:
+            if type(z) is not int and type(z) is not float:
                 raise TypeError(msg)
 
-    matrix_div = [[round(y / div, 2) for y in i] for i in matrix]
-    return matrix_div
+    division = [[round(z / div, 2) for z in x] for x in matrix]
+    return division
