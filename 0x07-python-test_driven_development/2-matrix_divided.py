2975dc13a2459f4c1fa2d95b263baa32bd512c1e
Andresmelek <candres.isaza@gmail.com>
2020-01-13 21:50:53 -0500

    Adding changes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,22 +16,37 @@ def matrix_divided(matrix, div):
     msg1 = "Each row of the matrix must have the same size"
     matriz = len(matrix[0])
     matrix_div = []
+
     if matrix is None:
         raise TypeError(msg)
+
+    if matrix == []:
+        raise TypeError(msg)
+
     if type(div) is not int or type(div) is not float:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    if type(matrix) is not int and type(matrix) is not list:
+
+    if type(matrix) is not int or type(matrix) is not list:
+        raise TypeError(msg)
+
+    if not isinstance(matrix[0], list):
         raise TypeError(msg)
 
     for x in range(len(matrix)):
+
+        if not isinstance(matriz[x], list)):
+            raise TypeError(msg)
+
         if len(matrix[x]) != matriz:
             raise TypeError(msg1)
 
     for x in matrix:
         if not isinstance(matrix, list):
             raise TypeError(msg)
+
         for y in x:
             if type(y) is not int or type(y) is not float:
                 raise TypeError(msg)
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,12 +6,40 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def check_max(self):
+    """
+    Unitestest cases finding the max integer
+    """
+    def test_max(self):
         """
-        Checks if gets the max number
+        Unitest cases positive numbers
         """
-        self.assertAlmostEqual(max_integer([18, 89, 05]), 89)
-        self.assertAlmostEqual(max_integer([-5, -1, -8]), -1)
-        self.assertAlmostEqual(max_integer([0, 0, 0]), 0)
+        self.assertEqual(max_integer([18, 2, 3, 89]), 89)
+        self.assertEqual(max_integer([1, 55, 13, 7]), 55)
+
+    def test_none(self):
+        """
+        Unitests with None return and raise None
+        """
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(), None)
+        self.assertRaises(TypeError, max_integer, None)
         self.assertAlmostEqual(max_integer([]), None)
-        self.assertAlmostEqual(max_integer(), None)
+
+    def test_float(self):
+        """
+        Unitests with floats values
+        """
+        self.assertAlmostEqual(max_integer([5.98, 7.95, 1.89]), 7.95)
+
+    def test_tupla(self):
+        """
+        Unitest with a tuple
+        """
+        self.assertAlmostEqual(max_integer((6, 12, 24)), 24)
+
+    def test_max_neg(self):
+        """
+        Unitest cases finding the max with negative numbers
+        """
+        self.assertEqual(max_integer([-4, -8, -3, -5]), -3)
+        self.assertEqual(max_integer([-14, 5, -7]), 5)
