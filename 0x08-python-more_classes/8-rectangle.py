e463240708841ff6b6a70d69d00c2ac44d21d736
Andresmelek <candres.isaza@gmail.com>
2020-01-14 22:37:33 -0500

    Fixing conditionals

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -64,9 +64,9 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) is not Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if Rectangle.area(rect_1) == Rectangle.area(rect_2):
             return rect_1
