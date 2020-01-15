3c321be8b2fbb1b944429df530e9aadf0046e84b
Andresmelek <candres.isaza@gmail.com>
2020-01-15 12:49:23 -0500

    Fixing output

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
         height = has the height of the rectangle. """
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -16,7 +16,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >=0")
@@ -24,11 +24,11 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -40,4 +40,4 @@ class Rectangle:
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__height * 2) + (self.__width * 2)
+        return ((self.__height * 2) + (self.__width * 2))
