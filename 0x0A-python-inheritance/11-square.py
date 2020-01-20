a78f926ecfd77a478816d10fd1d1dbd7467bae4a
Andresmelek <candres.isaza@gmail.com>
2020-01-20 17:01:48 -0500

    changing something

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,7 +17,7 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return ("[Square] {}/{}".format(self.__width, self.__height))
 
 
 class Square(Rectangle):
@@ -25,9 +25,4 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return ("[Square] {}/{}".format(self.__size, self.__size))
+        Rectangle.__init__(self, self.__size, self.__size)
