e2d79d311bbed0cf627dd5651e8a12458555ce28
Andresmelek <candres.isaza@gmail.com>
2020-01-20 17:28:09 -0500

    changing stuff

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,7 +17,8 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return ("[Square] {}/{}".format(self.__width, self.__height))
+        return "[" + self.__class__.__name__ + "]" + " " + \
+            str(self.__width) + "/" + str(self.__height)
 
 
 class Square(Rectangle):
