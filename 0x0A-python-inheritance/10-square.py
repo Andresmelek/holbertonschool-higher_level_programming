d1f3b8b4815f3a2f5d27ca97c6bf3002d613e2bc
Andresmelek <candres.isaza@gmail.com>
2020-01-21 00:15:52 -0500

    fixing outputs

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,25 +5,12 @@ Public methods instance
 """
 
 
-class Rectangle(BaseGeometry):
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return "[" + __class__.__name__ + "]" + " " + str(self.__width) \
-            + "/" + str(self.__height)
-
-
 class Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
         Rectangle.__init__(self, self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
