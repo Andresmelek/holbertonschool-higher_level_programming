3819a937a6cb72909eab6413a81be46c2331b8b8
Andresmelek <candres.isaza@gmail.com>
2020-01-21 20:32:58 -0500

    uploading scripts that all the arguments to a Python list

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,11 +9,12 @@ adds all arguments to a Python list, and then save them to a file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        list_to_json = load_from_json_file(add_item.json)
+try:
+    list_to_json = load_from_json_file("add_item.json")
+except:
+        list_to_json = []
 
+for i in range(1, len(argv)):
+    list_to_json.append(argv[i])
 
-    for i in range(1, len(argv)):
-        list_to_json.append(argv[i])
-
-    save_to_json_file(add_item.json)
+save_to_json_file(list_to_json, "add_item.json")
