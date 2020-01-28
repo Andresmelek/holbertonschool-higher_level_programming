f79f5ed7539106bf9cb49cf9e73baea381a23c0f
Andresmelek <candres.isaza@gmail.com>
2020-01-27 22:14:38 -0500

    str method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,8 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(' ' * self.__x, end='')
             print('#' * self.__width)
+
+    def __str__(self):
+        """ string method """
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format
+               (self.id, self.__x, self.__y, self.__width, self.__height))
