4e5acef39b6b940a33d26bfdeea9d918ce95dff4
Andresmelek <candres.isaza@gmail.com>
2020-01-27 22:29:51 -0500

    Fixing

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,39 +4,23 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Square class """
+    """ Class Square """
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """ square side """
+        """ size getter """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ square sizes """
+        """ size setter """
         self.width = value
         self.height = value
 
     def __str__(self):
         """ str method """
         return ("[Square] ({}) {}/{} - {}".format
-               (self.id, self.x, self.y, self.width))
-
-    def update(self, *args, **kwargs):
-        """ updated the square class """
-        list_a = ['id', 'size', 'x', 'y']
-        if args is not None and len(args) != 0:
-            for i in range(len(args)):
-                setattr(self, list_a[i], args[i])
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """ returns a dictionary representation """
-        keys = ['id', 'size', 'x', 'y']
-        values = [self.id, self.width, self.x, self.y]
-        return dict(zip(keys, values))
+                (self.id, self.x, self.y, self.width))
