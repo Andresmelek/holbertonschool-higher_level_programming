ab62740811457b7aee4f8e5c594593ed577836ca
Andresmelek <candres.isaza@gmail.com>
2020-03-03 15:37:12 -0500

    Fixing output

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+	state_id INT NOT NULL REFERENCES states,
+	INCLUDE FOREIGN KEY (state_id) REFERENCES cities(id),
 	name VARCHAR(256) NOT NULL
 );
