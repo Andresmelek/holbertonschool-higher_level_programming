c2eaf3592f0c232922f2306b7ab7443782498922
Andresmelek <candres.isaza@gmail.com>
2020-03-03 21:34:27 -0500

    Fixing output

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.title
-FROM tv_shows
-JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.id
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
-ON tv_genres.id = tv_show_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
