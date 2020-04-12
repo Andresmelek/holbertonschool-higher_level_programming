32aa938b5a8c2569b768d762b2032e408cab5958
Andresmelek <candres.isaza@gmail.com>
2020-04-12 11:48:43 -0500

    Puta

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#script that takes in a URL and displays all HTTP methods the server will accept.
+#shows all HTTP methods the server will accept.
 curl -sI $1 | grep Allow | cut -d ":" -f 2 | cut -c 2-
 
 
