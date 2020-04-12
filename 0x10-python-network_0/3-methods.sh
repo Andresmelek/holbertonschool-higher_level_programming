306ce2441cb510e022bb2b730a5678c22f6d2930
Andresmelek <candres.isaza@gmail.com>
2020-04-12 11:52:05 -0500

    Fixing

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#shows all HTTP methods the server will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f2-
-
+# displays all HTTP methods the server will accept.
+curl -sI $1 | grep Allow | cut -d ":" -f 2 | cut -c 2-
