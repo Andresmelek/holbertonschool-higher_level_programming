3271ccbe2615ae609f83b22ccd1191d7a9528cc3
Andresmelek <candres.isaza@gmail.com>
2020-04-12 11:46:41 -0500

    Fixing shiban

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 #script that sends a DELETE request to the URL passed
 curl -sX "DELETE" $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
-#!/bin/bas
+#!/bin/bash
 #script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI $1 | grep Allow | cut -d ":" -f 2 | cut -c 2-
 
