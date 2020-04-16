a4cd4065baa6907108fa40af51103c14e88e5e7f
andresmelek <candres.isaza@gmail.com>
2020-04-16 15:52:25 +0000

    fixing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 """ Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a
 parameter, and displays the body of the response
