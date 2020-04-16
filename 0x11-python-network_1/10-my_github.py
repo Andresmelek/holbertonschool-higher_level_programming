d7edfda0084a3f4ac2124f7a1d564f8d2735675b
andresmelek <candres.isaza@gmail.com>
2020-04-16 16:10:23 +0000

    pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    reque = reque.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    reque = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(reque.json().get('id'))
