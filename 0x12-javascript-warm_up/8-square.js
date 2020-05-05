08a8bac0d0f3a8fd83d6a529a94a5979ec346d80
andresmelek <candres.isaza@gmail.com>
2020-05-05 12:35:29 -0500

    Fixing requirement

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 let y = 0;
 const value = process.argv[2];
-if (value === undefined) console.log('Missing size');
+if (value === undefined || isNaN(value)) console.log('Missing size');
 while (y < value) {
   for (let i = 0; i < value; i++) {
     process.stdout.write('X');
