618a9e8ded3c312206a0b92a9e412c594d8b5da7
andresmelek <candres.isaza@gmail.com>
2020-05-05 12:38:09 -0500

    Fixing requirement

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ if (process.argv <= 3) {
   console.log(isNaN);
 } else {
   console.log(add(parseInt(process.argv[2], 10), parseInt(process.argv[3]), 10));
-}  
-function add(a, b) {
+}
+function add (a, b) {
   return a + b;
 }
