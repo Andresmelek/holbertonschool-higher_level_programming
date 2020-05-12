9cc9447cc75d5e74687f3219fb97116acc29eab8
andresmelek <candres.isaza@gmail.com>
2020-05-12 12:59:04 -0500

    Api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,6 @@ const argv = process.argv;
 const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`;
 
 request(url, function (error, response, body) {
-  if (error) console.log(error);
-  console.log(JSON.parse(body).title);
+  if (error) throw error;
+  else console.log(JSON.parse(body).title);
 });
